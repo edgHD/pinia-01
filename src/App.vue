b368b9bc90497247e0400049033b01ba0e371ddd
Edgar <edgaarr2005@hotmail.com>
2025-05-24 20:23:59 -0400

    Testing out Pinia with a DefinedStore: counter!

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <base-container title="Pinia"></base-container>
+  <TheCounter />
+  <button @click="counter.increment">Add 1</button>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import { counterStore } from './pinia/counter.js';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+  },
+  setup() {
+    const counter = counterStore();
+    return { counter };
   },
 };
 </script>
---
+++ src/components/TheCounter.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3>{{ counter.count }}</h3>
+</template>
+<script>
+import { counterStore } from '../pinia/counter.js';
+
+export default {
+    setup() {
+        const counter = counterStore();
+        return { counter };
+    },
+};
+</script>
